0f82b54d7a987c0df5f5f7e82d1fb16fd541727f
m6mmik <andreaskuuskaru99@hotmail.com>
2019-09-11 19:22:05 +0300

    tund2

---
+++ tund2/time.php
@@ -0,0 +1,26 @@
+<?php 
+	$userName = "Andreas Kuuskaru";
+	$fulltimenow = date("d,m,Y H:i:s");
+	$hournow = date("H");
+	$partofday = "Hagune aeg";
+
+?>
+<html lang="en">
+
+	<head>
+		<title>
+			<?php echo $userName;?>
+		</title>
+		
+	</head>
+
+	<body>
+        <div class="content">
+		<?php echo "<h1>Leht avati ". $fulltimenow. "</h1>";?>
+                
+            
+        </div>
+		
+	</body>
+	
+</html>

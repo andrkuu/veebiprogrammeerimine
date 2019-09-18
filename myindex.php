508e72cad44dc69b182d0a6090390e8c6081000c
Andreas Kuuskaru <andrkuu@users.noreply.github.com>
2019-09-18 13:50:50 +0300

    Pilt

---
+++ myindex.php
@@ -0,0 +1,69 @@
+<?php
+	$userName = "Andreas Kuuskaru";
+	$fullTimeNow = date("d.m.Y");
+
+    $hourNow = date("H");
+    $partOfDay = "hägune aeg";
+
+    if($hourNow <= 8){
+        $partOfDay = "varahommik";
+    }
+    else if ($hourNow > 8){
+        $partOfDay = "hommik";
+    }
+    else if ($hourNow > 12){
+        $partOfDay = "lõuna";
+    }
+    else if ($hourNow > 15){
+        $partOfDay = "õhtu";
+    }
+    else $partOfDay = "öö";
+
+    $semesterStart = new DateTime("2019-9-2");
+    $semesterEnd = new DateTime("2019-12-13");
+    $semesterDuration = $semesterStart -> diff($semesterEnd) -> days;
+
+    $today = new DateTime("now");
+    $fromSemesterStart = $semesterStart -> diff($today) -> days;
+
+    //echo($fromSemesterStart);
+    //echo("<br>".$semesterDuration);
+
+    $semesterInfoHtml = "<p>Info semestri kohta pole kättesaadav.</p>";
+
+    $semesterInfoHtml = "<p>Semester on täies hoos:";
+    $semesterInfoHtml .= '<meter min="0"';
+    $semesterInfoHtml .= ' max="'.$semesterDuration.'"';
+    $semesterInfoHtml .= ' value="'.$fromSemesterStart.'">';
+    $semesterInfoHtml .= ' </meter>';
+    $semesterInfoHtml .= "</p>";
+
+
+//var_dump($fromSemesterStart -> days);
+?>
+
+<!DOCTYPE html>
+<html lang="ET">
+    <head>
+        <meta charset="UTF-8">
+        <title>Veebi programeerimine 2019</title>
+    </head>
+
+    <body>
+        <h1>Andreas Kuuskaru</h1>
+        <p>See veebileht on valminud õppetöö käigus ning ei sisalda mingisugust tõsiselt võetavat sisu</p>
+
+	<?php
+        echo($semesterInfoHtml);
+		echo $userName;
+        echo "<p>Lehe avamise hetkel oli " .$fullTimeNow .", " .$partOfDay .".</p>";
+        //print_r($semesterStart);
+        //print_r( $semesterStart["timezone"]);
+
+	?>
+
+    <hr>
+    <img src="pildid/Tux.png" alt="juhuslik foto">
+</body>
+
+</html>

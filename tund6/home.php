60736127cc689e7ed11c9d045a68b24bc5b59c5a
Andreas Kuuskaru <andrkuu@users.noreply.github.com>
2019-10-09 13:02:09 +0300

    tund6

---
+++ tund6/home.php
@@ -0,0 +1,15 @@
+<?php
+  require("../../../config.php");
+  require("functions_main.php");
+  require("functions_user.php");
+  
+  $userName = "Sisselogimata kasutaja";
+
+  require ("header.php");
+
+  echo "<h1>" .$userName .", veebiprogrammeerimine 2019</h1>";
+  ?>
+  <p>See veebileht on valminud õppetöö käigus ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+
+</body>
+</html>
